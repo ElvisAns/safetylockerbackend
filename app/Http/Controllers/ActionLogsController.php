61218e30adbd6ff3d1104275721ec4a61d73e635
ElvisAns <ansimapersic@gmail.com>
2022-11-08 00:27:18 +0100

    The action log controller

---
+++ app/Http/Controllers/ActionLogsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\ActionLogs;
+use Exception;
+
+class ActionLogsController extends Controller
+{
+    public function save(Request $request){
+        $log = new ActionLogs();
+        $log->type = $request->input('type');
+        $log->info = $request->input('info');
+
+        try{
+            $log->save();
+            return ['message'=>"logs saved with success"];
+        }
+        catch(\Exception $e){
+            return ['error'=>'true','message'=>'Error inserting the log information'];
+        }
+    }
+
+    public function get_all(){
+        return ActionLogs::all();
+    }
+}
