11fc945e35382571d83e89c5bb62c9feb74ffd79
ElvisAns <ansimapersic@gmail.com>
2022-11-07 22:56:45 +0100

    Brainstorm the apis endpoint

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/lines/{line_id?}',function ($line_id=null){
+    if($line_id){
+        //return state of the current line
+    }
+    //return state of all lines
+});
+
+Route::post('/lines/{line_id}/{state}',function($line_id,$state){
+    //Requires the current password in the request
+    //put a given line in a given state (on and off)
+    //each action should put something in the log
+});
+
+Route::post('/lines/maintenance/{state}',function($state){
+    //start or stop maintence maintenance
+    //Require current password
+    //each action should put something in the log
+});
+
+Route::get('/auth/password/forgotten',function(){
+    //send email to admin with the current password to unclock maintenance mode
+    //each action should put something in the log
+});
+
+Route::post('/auth/password/get_new',function(){
+    //get new password from the backend
+    //must submit the old password though post
+    //each action should put something in the log
+});
+
+Route::get('/lines/logs',function(){
+    //get list of logs and timestamp per entry
 });
