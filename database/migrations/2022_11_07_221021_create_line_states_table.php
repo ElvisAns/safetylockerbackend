593a40914f92f523448ffb93430f0fff81d22c47
ElvisAns <ansimapersic@gmail.com>
2022-11-09 14:45:58 +0100

    Correctly write the migration file

--- database/migrations/2022_11_07_221021_create_line_states_table.php
+++ database/migrations/2022_11_07_221021_create_line_states_table.php
@@ -15,7 +15,8 @@ class CreateLineStatesTable extends Migration
     {
         Schema::create('lines_states', function (Blueprint $table) {
             $table->id();
-            $table->string('line_name');
+            $table->string('line_name')->unique();
+            $table->string('connected_pin')->unique();
             $table->boolean('state');
             $table->timestamps();
         });
